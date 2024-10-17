7d78aa157e2196c352f53681dc1f8984f2c61b04
birukzlab <birukalemu63@gmail.com>
2024-10-16 20:11:22 -0500

    Added more features

--- eisenhower_matrix_app/app.py
+++ eisenhower_matrix_app/app.py
@@ -1,29 +1,36 @@
-# Main Flask application file
+# app.py
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
+from utils.categorizer import categorize_tasks
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Replace with a secure random key
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        tasks = request.form.getlist('task')
-        categories = request.form.getlist('category')
-        matrix = {'urgent_important': [], 'important_not_urgent': [], 'urgent_not_important': [], 'not_urgent_not_important': []}
-
-        for task, category in zip(tasks, categories):
-            if category == 'urgent_important':
-                matrix['urgent_important'].append(task)
-            elif category == 'important_not_urgent':
-                matrix['important_not_urgent'].append(task)
-            elif category == 'urgent_not_important':
-                matrix['urgent_not_important'].append(task)
-            else:
-                matrix['not_urgent_not_important'].append(task)
-
-        return render_template('matrix.html', matrix=matrix)
-
+        # Retrieve tasks from the form
+        tasks = []
+        num_tasks = int(request.form['num_tasks'])
+        for i in range(num_tasks):
+            task = {
+                'title': request.form.get(f'title_{i}'),
+                'objective': request.form.get(f'objective_{i}'),
+                'importance': request.form.get(f'importance_{i}'),
+                'urgency': request.form.get(f'urgency_{i}')
+            }
+            tasks.append(task)
+        # Store tasks in session
+        session['tasks'] = tasks
+        return redirect(url_for('matrix'))
     return render_template('home.html')
+
+@app.route('/matrix')
+def matrix():
+    tasks = session.get('tasks', [])
+    # Categorize tasks into the Eisenhower Matrix
+    matrix = categorize_tasks(tasks)
+    return render_template('matrix.html', matrix=matrix)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
--- eisenhower_matrix_app/utils/categorizer.py
+++ eisenhower_matrix_app/utils/categorizer.py
@@ -1,1 +1,23 @@
 # Utility functions for categorizing tasks
+# utils/categorizer.py
+
+def categorize_tasks(tasks):
+    matrix = {
+        'do_first': [],        # Urgent and Important
+        'schedule': [],        # Not Urgent but Important
+        'delegate': [],        # Urgent but Not Important
+        'eliminate': []        # Neither Urgent nor Important
+    }
+    for task in tasks:
+        importance = task['importance'] == 'True'
+        urgency = task['urgency'] == 'True'
+
+        if importance and urgency:
+            matrix['do_first'].append(task)
+        elif importance and not urgency:
+            matrix['schedule'].append(task)
+        elif not importance and urgency:
+            matrix['delegate'].append(task)
+        else:
+            matrix['eliminate'].append(task)
+    return matrix
